ecd212b80b0af133eab33242f0eac803e098cea1
qxh <3048086150@qq.com>
2020-10-10 09:12:47 +0800

    20%

---
+++ Million-answers/million-answers.md
@@ -0,0 +1,13 @@
+//react项目
+create-react-app million
+
+//导入数据
+navicat-----json
+
+//服务器
+express
+mysql
+router
+
+//客户端---浏览器
+axios redux react-redux react-router-dom
---
+++ Million-answers/reactappmillion/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = require('./router')
+
+
+
+const app = express()
+
+
+app.use(router)
+
+
+app.listen(8080, function() {
+    console.log('running', 'http://localhost:8080/');
+})
+
+module.exports = app
+return ( <
+    div > 你好 < Button > Start < /Button> </div >
+)
+}
+}
+ReactDOM.render( < App / > , document.querySelector("#root"))
