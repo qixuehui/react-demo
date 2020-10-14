a70ad7279a9dfdf6786538e18d0b5178f65c4d71
qxh <3048086150@qq.com>
2020-10-14 11:57:01 +0800

    100%

--- Million-answers/server/router.js
+++ Million-answers/server/router.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const queryFn = require('./sql')
+const { queryUFn, insertUFn } = require('./user')
 
 
 const router = express.Router()
 
 
 router.get('/', function(req, res) {
-    res.send('这是答题API服务器')
-})
-
+        res.send('这是答题API服务器')
+    })
+    //请求题目
 router.get('/api/rtimu', async function(req, res) {
+        console.log(req.query.page);
+        //跨域解决
+        res.append("Access-Control-Allow-Origin", "*")
+        res.append("Access-Control-Allow-Content-Type", "*")
+        let page = req.query.page ? req.query.page : 2
+        let sql = `select * from quizzes limit ${page * 10},10`
+        const result = await queryFn(sql)
+            // res.send(result)
+        res.json(Array.from(result))
+    })
+    //注册
+router.post('/registered', async function(req, res) {
+
+        console.log(req.query.page);
+        //跨域解决
+        res.append("Access-Control-Allow-Origin", "*")
+        res.append("Access-Control-Allow-Content-Type", "*")
+        let page = req.query.page ? req.query.page : 2
+        let sql = `select * from quizzes limit ${page * 10},10`
+        const result = await queryFn(sql)
+            // res.send(result)
+        res.json(Array.from(result))
+    })
+    //登录
+router.post('/login', async function(req, res) {
 
     console.log(req.query.page);
     //跨域解决
